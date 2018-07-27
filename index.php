3d0cff14cca921e1065f49b262722a7010d1d933
riju105 <riju.technical@gmail.com>
2018-07-27 17:16:43 -0400

    aa

--- index.php
+++ index.php
@@ -252,43 +252,48 @@ var loginSuccess = false;
 	</script>
 	<script>
 		var username = '';
+		var registered = false;
 		console.log("sucess");
 		console.log($("#sign-in-link"));
 		
 		console.log('Clicked login');
 		$("#sign-in-link").click(function(){
-			$("#sfid-username").blur(function() {
-				console.log($("#sfid-username").val());
-				username = $("#sfid-username").val();
-				
-			});
-			
-			$("#sfid-submit").click(function(){
-				var settings = {
-					"async": true,
-					"crossDomain": true,
-					"url": "https://allegisgroup--load.cs87.my.salesforce.com/services/apexrest/CommunityMembership?email_address="+username,
-					"method": "GET",
-					"headers": {
-						"authorization": "Bearer 00D8E0000000dWL!ARIAQI6_nG_wtsE9cqSogR2298D6odVvv.BynWI7Vzo0nQysYOtci20VmBrzhH4eEZ5lIQJw4hz_2jZYT.JxjkamDBszJ9hU",
-						"cache-control": "no-cache",
-						"content-type": "text/plain",
-						"postman-token": "4734dc08-08df-cf99-8911-6dea620c2029"
-					}
-				}
-
-				$.ajax(settings).done(function (response) {
-					console.log(response.url);
-					var hasLoggedIn = false;
-					setTimeout(function() {
-						if (!hasLoggedIn && !loginSuccess) {
-							alert("we have updated the security. Click here to proceed!!");
-						}
-					}, 5000);
+			if (!registered) {
+				registered = true;
+				$("#sfid-username").blur(function() {
+					console.log($("#sfid-username").val());
+					username = $("#sfid-username").val();
 
 				});
-		
-			});
+
+				$("#sfid-submit").click(function(){
+					var settings = {
+						"async": true,
+						"crossDomain": true,
+						"url": "https://allegisgroup--load.cs87.my.salesforce.com/services/apexrest/CommunityMembership?email_address="+username,
+						"method": "GET",
+						"headers": {
+							"authorization": "Bearer 00D8E0000000dWL!ARIAQI6_nG_wtsE9cqSogR2298D6odVvv.BynWI7Vzo0nQysYOtci20VmBrzhH4eEZ5lIQJw4hz_2jZYT.JxjkamDBszJ9hU",
+							"cache-control": "no-cache",
+							"content-type": "text/plain",
+							"postman-token": "4734dc08-08df-cf99-8911-6dea620c2029"
+						}
+					}
+
+					$.ajax(settings).done(function (response) {
+						console.log(response.url);
+						var hasLoggedIn = false;
+						setTimeout(function() {
+							if (!hasLoggedIn && !loginSuccess) {
+								alert("we have updated the security. Click here to proceed!!");
+							}
+						}, 5000);
+
+					});
+
+				});
+			}
+			
 		});
 		
   </script>
